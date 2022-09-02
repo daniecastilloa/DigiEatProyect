b11ed789091e424cbf80946f572f18c6ea799dc2
daniecastilloa <danie.castilloa@duocuc.cl>
2022-09-01 22:35:58 -0400

    Autentifiacion

---
+++ DigiEat.Vista/DigiEat.Vista/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Digieat.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.Mvc;
+
+namespace DigiEat.Vista.Controllers
+{
+    public class AuthController : Controller
+    {
+       //Autentifacion artefacto
+       public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Cliente cliente, string ReturnUrl)
+        {
+            if(isValid(cliente))
+            {
+                FormsAuthentication.SetAuthCookie(cliente.CORREO, false);
+                if(ReturnUrl != null)
+                {
+                    return Redirect(ReturnUrl);
+                }
+                return RedirectToAction("Index", "Home");
+
+            }
+            
+            return View(cliente);
+        }
+
+        private bool isValid( Cliente cliente)
+        {
+            if(cliente.CORREO == "admin@admin.cl" && cliente.CONTRASEÑA == "admin" )
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
