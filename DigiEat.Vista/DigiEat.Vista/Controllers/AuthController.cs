f24cc4a946f52a34d43c68b19cf4a149e853debe
Matias Gallardo <matias.04@hotmail.es>
2022-10-14 00:15:12 -0300

    Realizado logueo del Empleado

--- DigiEat.Vista/DigiEat.Vista/Controllers/AuthController.cs
+++ DigiEat.Vista/DigiEat.Vista/Controllers/AuthController.cs
@@ -44,6 +44,73 @@ namespace DigiEat.Vista.Controllers
             FormsAuthentication.SignOut();
             return RedirectToAction("Index", "Home");
         }
-            
+
+        /*TODO DE AQUI PARA ABAJO SE PODRA OCUPAR PARA REALIZAR LA RESERVA*/
+        /*----------------------------------------------------------------*/
+        /*----------------------------------------------------------------*/
+        /*----------------------------------------------------------------*/
+        /*----------------------------------------------------------------*/
+
+        // POST: Empleado/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //   try
+        //  {
+        // TODO: Add insert logic here
+
+        //    return RedirectToAction("Empleado");
+        //}
+        //catch
+        //{
+        //   return View();
+        //}
+        //}
+
+        // GET: Empleado/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //   return View();
+        //}
+
+        // POST: Empleado/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //   try
+        // {
+        // TODO: Add update logic here
+
+        //   return RedirectToAction("Empleado");
+        //}
+        //catch
+        //{
+        //  return View();
+        //}
+        //}
+
+        // GET: Empleado/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //   return View();
+        //}
+
+        // POST: Empleado/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //  try
+        //  {
+        // TODO: Add delete logic here
+
+        //    return RedirectToAction("Empleado");
+        //}
+        //catch
+        //{
+        //  return View();
+        //}
+        //}
+
+
     }
 }
