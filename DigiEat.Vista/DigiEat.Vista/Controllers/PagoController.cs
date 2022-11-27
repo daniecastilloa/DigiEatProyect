eaaf8d3f8f30ce361a93893071f7838c0d46960c
Matias Gallardo <matias.04@hotmail.es>
2022-11-27 16:19:56 -0300

    Cambios visuales, registro y pago

---
+++ DigiEat.Vista/DigiEat.Vista/Controllers/PagoController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigiEat.Vista.Controllers
+{
+    public class PagoController : Controller
+    {
+        // GET: Pago
+        public ActionResult Pago()
+        {
+            return View();
+        }
+
+        // GET: Pago/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Pago/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pago/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Pago");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pago/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Pago/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Pago");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pago/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Pago/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Pago");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
